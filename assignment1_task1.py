f8b0098b51269b1c5c042ddd4799e785b29a5007
arnav-dev001 <arnavchandanshiv01@gmail.com>
2025-05-15 17:15:37 +0530

    Assignment 1 submission

---
+++ assignment1_task1.py
@@ -0,0 +1,10 @@
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+a = int(a)
+b = int(b)
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print('\nAddition: ' ,add ,'\nSubtraction: ' ,sub ,'\nMultiplication: ' ,mul ,'\nDivision' ,div)
---
+++ assignment1_task2.py
@@ -0,0 +1,5 @@
+name = input('Enter your first name: ')
+surname = input('Enter your last name: ')
+fullname = name + ' ' + surname
+
+print('\nHello, ' + fullname + '! Welcome to the Python program.')
